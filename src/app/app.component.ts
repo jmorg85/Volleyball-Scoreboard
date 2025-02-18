d75169b17c79aa69725c1b946d34be9b18ca9026
jmorg85 <justicemorgan147@gmail.com>
2025-02-17 20:33:16 -0600

    I finished the scoreboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Volleyball-Scoreboard';
+  team1Score: number = 0
+  team2Score: number = 0
+
+  incrementScore(teamName: string): void
+  {
+    if(teamName == 'team1') {this.team1Score++;}
+    else {this.team2Score++;}
+  }
+
+  decrementScore(teamName: string): void
+  {
+    if(teamName == 'team1') {
+      if(this.team1Score != 0) {this.team1Score--;}
+    }
+    else {
+      if(this.team2Score != 0) {this.team2Score--;}
+    }
+  }
+
+  resetScores(): void
+  {
+    this.team1Score = 0, this.team2Score = 0;
+  }
 }
