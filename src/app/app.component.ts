d72187b8199d9ebc47c1e6581f10002f8e77a283
jmorg85 <justicemorgan147@gmail.com>
2026-05-19 11:20:18 -0500

    Added new funtionality to change the wallpaper of the scoreboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
@@ -13,6 +13,20 @@ export class AppComponent {
   title = 'Volleyball-Scoreboard';
   team1Score: number = 0
   team2Score: number = 0
+
+  constructor(private renderer: Renderer2) {
+    this.setBodyBackground("assets/images/haikyuu_season_1_background.webp");
+  }
+
+  setBodyBackground(imageURL: string) {
+    this.renderer.setStyle(document.body, 'background', `url(${imageURL})`);
+    this.renderer.setStyle(document.body, 'backgroundRepeat', 'no-repeat');
+    this.renderer.setStyle(document.body, 'backgroundPosition', 'center');
+    this.renderer.setStyle(document.body, 'backgroundAttachment', 'center fixed');
+    this.renderer.setStyle(document.body, 'backgroundSize', 'cover');
+    this.renderer.setStyle(document.body, 'minHeight', '100vh');
+    this.renderer.setStyle(document.body, 'width', '100%');
+  }
 
   incrementScore(teamName: string): void
   {
